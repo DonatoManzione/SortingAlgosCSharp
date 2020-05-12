77c5d7e37b2d1732ef6f5ba976ba18c4acfb4ffa
Edda <edda@dms-MacBook-Pro.local>
2020-05-12 09:52:31 -0400

    First commit- implementation of bubble sort

---
+++ SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace SortingAlgorithms
+{
+    public class BubbleSort
+    {
+        int[] Arr { get; set; }
+        public BubbleSort(int[] unsortedArr)
+        {
+            Arr = unsortedArr;
+        }
+
+
+        public int[] Sort()
+        {
+            for(int i = 0; i < Arr.Length-1; i++)
+            {
+                for(int j = i + 1; j < Arr.Length; j++)
+                {
+                    if (Arr[i] > Arr[j])
+                    {
+                        Swap(i, j);
+                    }
+                }
+            }
+            return Arr;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            int temp = Arr[firstIndex];
+            Arr[firstIndex] = Arr[secondIndex];
+            Arr[secondIndex] = temp;
+        }
+
+        public void PrintArray()
+        {
+            Console.Write("{ ");
+            for(int i = 0; i < Arr.Length - 1; i++)
+            {
+                Console.Write(Arr[i] + ", ");
+            }
+            Console.WriteLine(Arr.Length - 1+" }");
+        }
+    }
+}
---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 8, 5, 2, 7, 5, 1, 6, 9 };
+            BubbleSort bubble = new BubbleSort(arr);
+            bubble.Sort();
+            bubble.PrintArray();
+        }
+    }
+}
